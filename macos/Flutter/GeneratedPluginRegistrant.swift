ded15c6830054e3412bd8f3515b23e9a92a6cd8c
Nahid Hasan Noyon <nahid.inflack@gmail.com>
2025-05-18 15:03:37 +0600

    Add CMake and Podfile configurations for Flutter project
    - Created build_file_index.txt for CMake build system.
    - Added cmake_install.cmake to manage installation of build artifacts.
    - Introduced configure_fingerprint.bin for structured logging of build configurations.
    - Generated metadata_generation_command.txt for CMake metadata.
    - Added prefab_config.json to manage package configurations.
    - Created symbol_folder_index.txt for symbol folder management.
    - Initialized Podfile for iOS with necessary configurations and dependencies.
    - Initialized Podfile for macOS with necessary configurations and dependencies.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import no_screenshot
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  NoScreenshotPlugin.register(with: registry.registrar(forPlugin: "NoScreenshotPlugin"))
 }
